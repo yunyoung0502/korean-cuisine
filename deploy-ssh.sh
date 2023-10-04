967488e71f07397862e428707955769dd8403214
dhchoi-lazy <135495811+dhchoi-lazy@users.noreply.github.com>
2023-10-04 15:20:41 +0000

    Update deploy-ssh.sh

--- deploy-ssh.sh
+++ deploy-ssh.sh
@@ -1,2 +1,31 @@
 #!/bin/bash
-git pull origin main
+
+# Define the local directory where your repository exists.
+LOCAL_REPO_DIR="."
+
+# Function to perform a git pull.
+git_pull() {
+  echo "Attempting git pull..."
+  git pull origin main
+}
+
+# Navigate to the local repository directory.
+if [ -d "$LOCAL_REPO_DIR" ]; then
+  cd "$LOCAL_REPO_DIR" || exit 1
+  
+  # Try to pull the latest changes from the remote repository.
+  if git_pull; then
+    echo "Git pull successful."
+  else
+    echo "Git pull failed. Discarding local changes and pulling anew."
+    
+    # Discard local changes and uncommitted files.
+    git reset --hard HEAD
+    git clean -fd
+    
+    # Perform a git pull again.
+    git_pull
+  fi
+else
+  echo "Local repository directory does not exist."
+fi
