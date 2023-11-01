45da0952a723d2182a592f61b41271b755aeb066
nerdinary2 <135495811+dhchoi-lazy@users.noreply.github.com>
2023-11-01 18:21:37 +0900

    git json server init

--- deploy-ssh.sh
+++ deploy-ssh.sh
@@ -9,6 +9,12 @@ git_pull() {
   git pull origin main
 }
 
+# Function to execute Python script.
+run_python_script() {
+  echo "Executing Python script..."
+  python3 merge_json.py
+}
+
 # Navigate to the local repository directory.
 if [ -d "$LOCAL_REPO_DIR" ]; then
   cd "$LOCAL_REPO_DIR" || exit 1
@@ -16,6 +22,14 @@ if [ -d "$LOCAL_REPO_DIR" ]; then
   # Try to pull the latest changes from the remote repository.
   if git_pull; then
     echo "Git pull successful."
+    
+    # Execute the Python script here.
+    if run_python_script; then
+      echo "Python script executed successfully."
+    else
+      echo "Python script execution failed."
+    fi
+
   else
     echo "Git pull failed. Discarding local changes and pulling anew."
     
